79f81c9bbb6249afb7b76f20102212062aa6cb71
AquaUseful <l.vladik.m@gmail.com>
2022-11-26 00:26:34 +0300

    Add more validation to registration form

--- backend/src/main/java/org/psu/lab5/pojo/RegisterRequest.java
+++ backend/src/main/java/org/psu/lab5/pojo/RegisterRequest.java
@@ -1,5 +1,7 @@
 package org.psu.lab5.pojo;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 import org.apache.logging.log4j.message.Message;
@@ -14,9 +16,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotEmpty(message = "Пустое имя пользоватея")
     @Pattern(regexp = "^[0-9A-Za-z]{5,20}$", message = "От 5 до 20 символов, только латинские буквы, цифры и _")
     private String username;
 
+    @NotEmpty(message = "Пустой пароль")
     @Pattern(regexp = "^(\\S){8,32}$", message = "От 8 до 32 непробельных символов")
     private String password;
+
+    @NotEmpty(message = "Пустой email")
+    @Email(message = "Недопустимый email адрес")
+    private String email;
 }
